5e5bda25eeae12aeb19902669548dbe45e16741a
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-03-26 01:49:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
+# def x = 1
 
-def countdown
+# while x < 10
+# puts "#{x} SECONDS!"
+# x -= 1
+# end 
+# puts "HAPPY NEW YEAR!"
+def countdown(x)
+  while x > 0
+ puts "#{x} SECOND(S)!"
+ x -= 1
+end 
+ return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  while x > 0
+ puts "#{x} SECOND(S)!"
+ sleep(1)
+ x -= 1
+end 
+ return "HAPPY NEW YEAR!"
 end
