b57388fefbd2c32c85cfa7c33d2b20e73a2abb78
Tanajah Rosenbaum <tanajahr25@gmail.com>
2020-03-26 01:36:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,8 +8,8 @@
 # puts "HAPPY NEW YEAR!"
 def countdown(x)
   while x < 10
-# puts "#{x} SECONDS!"
-# x -= 1
-# end 
-# puts "HAPPY NEW YEAR!"
+ puts "#{x} SECONDS!"
+ x -= 1
+end 
+ puts "HAPPY NEW YEAR!"
 end
